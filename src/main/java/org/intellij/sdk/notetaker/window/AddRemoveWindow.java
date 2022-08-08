361d1bf7d944d6f5da5593feea519f1bafe9d21e
Xiuyuan Qiu <xiuyuan206@gmail.com>
2022-08-08 11:06:35 -0700

    Working model for adding notes (uses separate list)

--- src/main/java/org/intellij/sdk/notetaker/window/AddRemoveWindow.java
+++ src/main/java/org/intellij/sdk/notetaker/window/AddRemoveWindow.java
@@ -1,26 +1,44 @@
 package org.intellij.sdk.notetaker.window;
 
-import com.intellij.ui.AnActionButton;
-import com.intellij.ui.AnActionButtonRunnable;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
 import com.intellij.ui.CollectionListModel;
 import com.intellij.ui.ToolbarDecorator;
 import com.intellij.ui.components.JBList;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.ContentFactory;
+import com.intellij.ui.content.ContentManager;
+import org.intellij.sdk.notetaker.storage.NoteModel;
+import org.intellij.sdk.notetaker.storage.NoteStorageManager;
 
 import javax.swing.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class AddRemoveWindow {
     private CollectionListModel<String> noteNames;
-    private JBList list;
+    private JBList<String> list;
 
-    public AddRemoveWindow() {
+    private List<NoteModel> noteList;
+    NoteStorageManager storageManager;
+    private Project project;
+
+    public AddRemoveWindow(Project project) {
+        this.project = project;
         noteNames = new CollectionListModel<>();
-        list = new JBList(noteNames);
-        noteNames.add("note1");
-        noteNames.add("note2");
+        storageManager = new NoteStorageManager(project);
+        noteList = storageManager.getNoteList();
+
+
+        if (noteList != null) {
+            noteList.stream().forEach(model -> {noteNames.add(model.getName());});
+        }
+        list = new JBList<>(noteNames);
     }
 
     public JComponent getComponent() {
@@ -50,11 +68,29 @@ public class AddRemoveWindow {
     }
 
     public void addNoteAction() {
-        noteNames.add(getRandomString());
+        NoteModel newNote = new NoteModel(getRandomString(), "");
+        if (noteList == null) {
+            ArrayList<NoteModel> newList = new ArrayList<>();
+            storageManager.setNoteList(newList);
+        }
+        noteList.add(newNote);
+        noteNames.add(newNote.getName());
+        updateNotesWindow(newNote);
     }
     public void removeNoteAction() {
         if (list.getSelectedIndex() != -1) {
             noteNames.remove(list.getSelectedIndex());
+        }
+    }
+
+    public void updateNotesWindow(NoteModel note) {
+        ToolWindow toolWindow = ToolWindowManager.getInstance(project).getToolWindow("Notetaker");
+        if (toolWindow != null) {
+            ContentFactory contentFactory = ContentFactory.SERVICE.getInstance();
+            ContentManager cm = toolWindow.getContentManager();
+            NoteWindow noteWindow = new NoteWindow(toolWindow, project, note);
+            Content noteTab = contentFactory.createContent(noteWindow.getContent(), note.getName(), false);
+            cm.addContent(noteTab);
         }
     }
 
