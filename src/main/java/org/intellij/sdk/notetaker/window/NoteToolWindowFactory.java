a439ab086988b308a7d750d577c3abcddcb51c62
Xiuyuan Qiu <xiuyuan206@gmail.com>
2022-08-09 11:53:27 -0700

    Renamed MyToolWindowFactory to NoteToolWindowFactory

--- src/main/java/org/intellij/sdk/notetaker/window/NoteToolWindowFactory.java
+++ src/main/java/org/intellij/sdk/notetaker/window/NoteToolWindowFactory.java
@@ -3,26 +3,19 @@ package org.intellij.sdk.notetaker.window;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
-import com.intellij.openapi.wm.ToolWindowManager;
-import com.intellij.ui.CollectionListModel;
-import com.intellij.ui.ToolbarDecorator;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentFactory;
 import com.intellij.ui.content.ContentManager;
 
-import com.intellij.ui.table.JBTable;
-import com.intellij.ui.table.TableView;
-import com.intellij.ui.tabs.impl.JBTabsImpl;
 import org.intellij.sdk.notetaker.storage.NoteModel;
 import org.intellij.sdk.notetaker.storage.NoteStorageManager;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class MyToolWindowFactory implements ToolWindowFactory {
+public class NoteToolWindowFactory implements ToolWindowFactory {
 
     /**
      * Create the tool window content.
